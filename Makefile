b45eea9dbd7a211d01919b5b21dfb853e597530d
Vibhash Kumar Singh <singh@cqse.eu>
2024-01-05 10:32:08 +0100

    Fetch project name from package.json in smoke.sh

--- Makefile
+++ Makefile
@@ -6,7 +6,6 @@ report:
 	echo "Generating report ..."
 	cd $(INPUT_DIR) && pdflatex -output-directory=$(OUTPUT_DIR) main.tex
 
-
 .PHONY: clean
 clean:
 	echo "Removing build files ..."
--- smoke.sh
+++ smoke.sh
@@ -1,3 +1,4 @@
 JSON_FILE=/JSONSchemaDiscovery/package.json
+NAME=$(node -pe "JSON.parse(fs.readFileSync('$JSON_FILE', 'utf8')).name")
 VERSION=$(node -pe "JSON.parse(fs.readFileSync('$JSON_FILE', 'utf8')).version")
-echo "JSONSchemaDiscovery $VERSION"
+echo $NAME $VERSION
