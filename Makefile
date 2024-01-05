dd97c2734a2b694ba73543b7da51f280eb124cf5
Vibhash Kumar Singh <singh@cqse.eu>
2024-01-05 20:14:13 +0100

    Add license

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
+# Copyright 2024, Vibhash Kumar Singh <singh13@ads.uni-passau.de>
+# SPDX-License-Identifier: GPL-2.0-only
+
 REPORT_DIR=report
 
 .PHONY: report
--- smoke.sh
+++ smoke.sh
@@ -1,3 +1,6 @@
+# Copyright 2024, Vibhash Kumar Singh <singh13@ads.uni-passau.de>
+# SPDX-License-Identifier: GPL-2.0-only
+
 JSON_FILE=/JSONSchemaDiscovery/package.json
 NAME=$(node -pe "JSON.parse(fs.readFileSync('$JSON_FILE', 'utf8')).name")
 VERSION=$(node -pe "JSON.parse(fs.readFileSync('$JSON_FILE', 'utf8')).version")
